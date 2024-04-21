f3a5cff6f615dd448d87e1fd2d83dc911cdd5b68
primboi <81822069+primitt@users.noreply.github.com>
2024-04-20 22:56:48 -0700

    commit

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import pythonosc as osc
 from pythonosc.dispatcher import Dispatcher
 from typing import List, Any
-
+from pythonosc.udp_client import SimpleUDPClient
+import os
+from dotenv import load_dotenv 
 #
 dispatcher = Dispatcher()
+load_dotenv()
 
-ip = "192.168.86.32" # set to the IP of the computer running the server and set the multi play ip to physical
+ip = os.getenv("ip") # set to the IP of the computer running the server and set the multi play ip to physical
 port = 8000 # set to the port the server is running on
 
 # Set up server and client for testing
-from pythonosc.udp_client import SimpleUDPClient
 
 client = SimpleUDPClient(ip, port)
 
-# Send message and receive exactly one message (blocking)
-response = client.send_message("cue/1/start", 1)
+response = client.send_message("cue/current/start", 1)
 print(response)
