b1b6c129d29a2651daaa82e9b763606ef8cc0b64
primboi <81822069+primitt@users.noreply.github.com>
2024-04-20 23:27:49 -0700

    waaasd

--- main2.py
+++ main2.py
@@ -22,7 +22,7 @@ def signal_handler(sig, frame):
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 def handle_response(unused_addr, *args):
-    print("Received response:", args)
+    print("\nReceived response:", args)
 
 local_dispatcher = Dispatcher()
 local_dispatcher.map("/status/current/qdesc", handle_response)
@@ -33,5 +33,21 @@ local_server_thread.start()
 
 client = SimpleUDPClient(ip, port)
 while True:
-    message = input("Enter a message: ")
+    message = input("Enter command: ")
+    msg_parts = message.split(" ")
+    if msg_parts[0] == "ADVANCE":
+        if msg_parts[1] == "cue":
+            print("Advancing cue")
+            client.send_message("/select/next", 1)
+    if msg_parts[0] == "BACK":
+        if msg_parts[1] == "cue":
+            print("Going back cue")
+            client.send_message("/select/prev", 1)
+    if msg_parts[0] == "GO":
+        print("Starting cue")
+        client.send_message("/cue/current/start", 1)
+    if msg_parts[0] == "STOP":
+        print("Stopping cue")
+        client.send_message("/cue/current/stop", 1)
+
     rsp = client.send_message(message, 1)
