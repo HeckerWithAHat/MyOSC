f344b96ec79fee9142abf08a2587a5f5dc99bb47
HeckerWithAHat <aryanj1010@gmail.com>
2024-04-20 22:41:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from typing import List, Any
 #
 dispatcher = Dispatcher()
 
-
+ip = "192.168.86.32" # set to the IP of the computer running the server and set the multi play ip to physical
+port = 8000 # set to the port the server is running on
 
 # Set up server and client for testing
 from pythonosc.udp_client import SimpleUDPClient
 
-client = SimpleUDPClient("127.0.0.1", 8000)
+client = SimpleUDPClient(ip, port)
 
 # Send message and receive exactly one message (blocking)
-response = client.send_message("cue/go", 1)
+response = client.send_message("cue/1/start", 1)
 print(response)
