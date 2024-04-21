07a1d50cac68c66536027fda2fba186768be851a
HeckerWithAHat <aryanj1010@gmail.com>
2024-04-20 23:16:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,40 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
 
-dispatcher = Dispatcher()
-load_dotenv()
-
-ip = os.getenv("ip") # set to the IP of the computer running the server and set the multi play ip to physical
+ip = "192.168.86.32" # set to the IP of the computer running the server and set the multi play ip to physical
 port = 8000 # set to the port the server is running on
 
 # Set up server and client for testing
 from pythonosc.udp_client import SimpleUDPClient
-
+from pythonosc.osc_server import AsyncIOOSCUDPServer    
+client = SimpleUDPClient(ip, port)
 
 # Send message and receive exactly one message (blocking)
-response = client.send_message("cue/1/start", 1)
-print(response)
+client.send_message("cue/1/start", 1)
+
+
+def filter_handler(address, *args):
+    print(f"{address}: {args}")
+
+
+dispatcher = Dispatcher()
+dispatcher.map("/cue/current/go", filter_handler)
+
+
+
+async def loop():
+    """Example main loop that only runs for 10 iterations before finishing"""
+    for i in range(10):
+        await asyncio.sleep(1)
+
+
+async def init_main():
+    server = AsyncIOOSCUDPServer((ip, port+1), dispatcher, asyncio.get_event_loop())
+    transport, protocol = await server.create_serve_endpoint()  # Create datagram endpoint and start serving
+
+    await loop()  # Enter main loop of program
+
+    transport.close()  # Clean up serve endpoint
+
+
+asyncio.run(init_main())
