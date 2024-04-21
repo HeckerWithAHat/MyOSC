7fcada6c3d2c5fb595c1976247be93ceef1c1cbe
HeckerWithAHat <aryanj1010@gmail.com>
2024-04-20 23:46:18 -0700

    convert main2 to main and delete other main

--- main.py
+++ main.py
@@ -1,45 +1,62 @@
 import pythonosc as osc
 from pythonosc.dispatcher import Dispatcher
 from typing import List, Any
-import asyncio
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning) 
-
-
-ip = "192.168.86.32" # set to the IP of the computer running the server and set the multi play ip to physical
-port = 8000 # set to the port the server is running on
-
-# Set up server and client for testing
 from pythonosc.udp_client import SimpleUDPClient
-from pythonosc.osc_server import AsyncIOOSCUDPServer    
+from pythonosc import osc_server
+import os
+from dotenv import load_dotenv
+import threading
+import time
+import signal
+import sys
+#
+load_dotenv()
+
+ip = os.getenv("ip")
+port = 8000
+
+def signal_handler(sig, frame):
+    print('\nShutting Down OSC!')
+    local_server.shutdown()
+    local_server_thread.join()
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+def handle_response(unused_addr, *args):
+    print("\nReceived response:", args)
+
+local_dispatcher = Dispatcher()
+local_dispatcher.map("/status/current/qdesc", handle_response)
+local_server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 2000), local_dispatcher)
+
+local_server_thread = threading.Thread(target=local_server.serve_forever)
+local_server_thread.start()
+
 client = SimpleUDPClient(ip, port)
-
-# Send message and receive exactly one message (blocking)
-client.send_message("cue/1/start", 1)
-
-
-def filter_handler(address, *args):
-    print(f"{address}: {args}")
-
-
-dispatcher = Dispatcher()
-dispatcher.map("/cue/current/go", filter_handler)
-
-
-
-async def loop():
-    """Example main loop that only runs for 10 iterations before finishing"""
-    for i in range(10):
-        await asyncio.sleep(1)
-
-
-async def init_main():
-    server = AsyncIOOSCUDPServer((ip, port+1), dispatcher, asyncio.get_event_loop())
-    transport, protocol = await server.create_serve_endpoint()  # Create datagram endpoint and start serving
-
-    await loop()  # Enter main loop of program
-
-    transport.close()  # Clean up serve endpoint
-
-
-asyncio.run(init_main())
+while True:
+    message = input("Enter command: ")
+    msg_parts = message.split(" ")
+    match msg_parts[0]:
+        case "ADVANCE":
+            print("Advancing cue")
+            client.send_message("/select/next", 1)
+        case "BACK":
+            if msg_parts[1] == "cue":
+                print("Going back cue")
+                client.send_message("/select/prev", 1)
+        case "GO":
+            if (len(msg_parts) == 2): 
+                print("Starting cue " + msg_parts[1])
+                client.send_message("/cue/" + msg_parts[1] + "/start", 1)
+            else:
+                print("Starting current cue")
+                client.send_message("/cue/current/start", 1)
+        case "STOP":
+            if (len(msg_parts) == 2): 
+                print("Stopping cue " + msg_parts[1])
+                client.send_message("/cue/" + msg_parts[1] + "/stop", 1)
+            else:
+                print("Stopping current cue")
+                client.send_message("/cue/current/stop", 1)
+        case _  :
+            print("Invalid command")
+            continue
